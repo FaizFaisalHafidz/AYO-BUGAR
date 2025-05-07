dc1e3f8a727c8a05276c476cdae8a750d5cfdc08
Wira Sanjaya <wirasanjaya142@gmail.com>
2025-05-07 13:33:39 +0700

    feat:make model
    outlet, card member, attendance member

---
+++ app/Http/Controllers/AttendanceMemberController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AttendanceMember;
+use Illuminate\Http\Request;
+
+class AttendanceMemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AttendanceMember $attendanceMember)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AttendanceMember $attendanceMember)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AttendanceMember $attendanceMember)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AttendanceMember $attendanceMember)
+    {
+        //
+    }
+}
