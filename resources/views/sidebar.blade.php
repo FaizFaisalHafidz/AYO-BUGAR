c7f3cc12aa25fd6c4d0584a76d55b5500b8f1f93
SulthonAuliya <sulthon.mt@gmail.com>
2025-05-08 00:28:28 +0700

    feat : transaksi

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -2,22 +2,21 @@
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-dark opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand px-4 py-3 m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard " target="_blank">
-        <span class="ms-1 text-sm text-dark">Creative Tim</span>
+        <span class="ms-1 text-sm text-dark">AYO-BUGAR</span>
       </a>
     </div>
     <hr class="horizontal dark mt-0 mb-2">
     <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active bg-gradient-dark text-white" href="../pages/dashboard.html">
+          <a class="nav-link text-dark" href="../pages/dashboard.html">
             <i class="material-symbols-rounded opacity-5">dashboard</i>
             <span class="nav-link-text ms-1">Dashboard</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-dark" href="../pages/tables.html">
-            <i class="material-symbols-rounded opacity-5">table_view</i>
-            <span class="nav-link-text ms-1">Tables</span>
+          <a class="nav-link text-dark {{ request()->routeIs('transaction.*') ? ' active' : '' }}" href="{{ route('transaction.index')}}">
+            <span class="nav-link-text ms-1">Transaksi</span>
           </a>
         </li>
         <li class="nav-item">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+
+Route::prefix('/transaction')->group( function(){
+    Route::get('/', [TransactionController::class, 'index'])->name('transaction.index');
+    Route::post('/store', [TransactionController::class, 'store'])->name('transaction.store');
+    Route::get('/kartu', [TransactionController::class, 'kartu'])->name('transaction.kartu');
+    Route::post('/kartu-store', [TransactionController::class, 'kartuStore'])->name('transaction.kartuStore');
+});
