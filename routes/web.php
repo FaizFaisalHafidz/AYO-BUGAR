ebccc0e95e30eaa6fe4b2bb7d8b1f3c35fc915dc
Wira Sanjaya <wirasanjaya142@gmail.com>
2025-05-08 07:20:30 +0700

    feat:authentification user

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\OutletController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login/authentification', [AuthController::class, 'authentification'])->name('authentification');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::prefix('/outlet')->group(function () {
     Route::get('/', [OutletController::class, 'index'])->name('outlet.index');
     Route::get('/add-new', [OutletController::class, 'create'])->name('outlet.craete');
